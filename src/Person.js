5b239420ac2cecf5e30e97d62f48559a38ad6727
Ashley Khng <ash15khng@gmail.com>
2020-04-12 10:36:54 +0800

    Added button and modified Person.js

--- src/Person.js
+++ src/Person.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const Person = ({name, age}) => {
+const Person = ({name, age, family, image}) => {
     return (
     <div>
-        <h1>Hello World!</h1>
-        <p>Name: {name}</p>
+        <h1>Name: {name}</h1>
         <p>Age: {age}</p>
+        <ul>
+            {
+                family.map((elem) => {
+                    return <li key={elem}>{elem}</li>;
+                })
+            }
+        </ul>
+        <img src={image} alt={name} width="200" />
     </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -20,41 +20,33 @@ import Person from "./Person";
 const root = document.getElementById("root");
 
 let person = [{
-  name: "Ashley",
-  age: 15
+  name: "Someone",
+  age: 10,
+  family: ["mother", "father"],
+  image: "https://randomwordgenerator.com/img/picture-generator/tree-736885_640.jpg"
 },
 {
-  name: "Someone",
-  age: 100
+  name: "Someone else",
+  age: 100,
+  family: ["mother", "father", "brother"],
+  image: "https://randomwordgenerator.com/img/picture-generator/tree-736885_640.jpg"
 }];
 
-let person2 = {
-  name: "Someone",
-  age: 100,
-}
-
-// const Person = ({name, age}) => {
-//   return (
-//     <div>
-//       <h1>Hello World!</h1>
-//       <p>Name: {name}</p>
-//       <p>Age: {age}</p>
-//     </div>
-//   );
-// };
-
-
-
 const App = () => {
+  let handleClick = () => {
+    console.log("clicked button");
+  }
+
   return (
     <div>
       {
         person.map((elem) => {
           return (
-            <Person name={elem.name} age={elem.age} />
+            <Person name={elem.name} age={elem.age} family={elem.family} image={elem.image} key={elem.name} />
           )
         })
       }
+      <button onClick={handleClick}>I am a button</button>
     </div>
   );
 }
