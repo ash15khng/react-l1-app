26f09483665441c3f4e19a3510f7968ad23cf30f
Ashley Khng <ash15khng@gmail.com>
2020-04-26 09:40:25 +0800

    kind of fixed Calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,11 +4,25 @@ const Calculator = () => {
   let [answer, setAnswer] = useState(0);
   let [input, setInput] = useState("");
   function calculate() {
-    let newInput = input.replace("/^(0+)|[\+\-\*\/](0)/", "")
+    let newInput = input.replace("/^(0+)|[\+\-\*\/](0)/", "");
+    setInput("0");
     return setAnswer(eval(newInput));
   }
+  function keypress(key) {
+    if ((key.keyCode >= 48 && key.keyCode <= 57) || key.keyCode === 187 || key.keyCode === 189 || key.keyCode === 191) {
+      if (input === 0) {
+          setInput(key.key);
+      } else {
+        setInput(input += key.key);
+      }
+    } else if (key.keyCode === 8 && input.length > 0) {
+      setInput(input.slice(0, -1) || 0);
+    } else if (key.keyCode === 13) {
+      calculate();
+    }
+  }
   return (
-    <div>
+    <div onKeyDown={keypress} tabIndex="-1">
       <h2>{answer}</h2>
       <h1>{input}</h1>
       <button onClick={() => setInput(input += "1")}>1</button>
