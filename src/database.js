b5ab04b15a7b1fd3a4eb83b7a6a4c38920bae1c4
Ashley Khng <ash15khng@gmail.com>
2020-05-10 09:34:40 +0800

    Added firebase config

---
+++ src/database.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAJ6fK0cwSEkDuVfyhaCO2ISPgS40FKgY",
+  authDomain: "principlesx-react.firebaseapp.com",
+  databaseURL: "https://principlesx-react.firebaseio.com",
+  projectId: "principlesx-react",
+  storageBucket: "principlesx-react.appspot.com",
+  messagingSenderId: "147587364060",
+  appId: "1:147587364060:web:6e476a47e878f649873019",
+  measurementId: "G-1RRLPEJNW3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
